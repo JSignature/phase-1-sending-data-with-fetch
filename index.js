fc8f4bf15d6178bc8dc2a17e1d87c4371c721b24
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-06 21:57:57 -0400

    work on test 3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   };
+
+const submitData = (userName, userEmail) => {
+    const data = {
+        name : userName,
+        email : userEmail
+    }
+    
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+         "Accept": "application/json",
+    },
+    body: JSON.stringify(data)
+})
+    .then(resp => resp.json())
+    .then(object => {
+        const element = document.createElement("h1")
+        element.textContent = object.id
+        document.querySelector("body").appendChild(element)
+
+    
+
+    })
+
+}
+submitData("Jimmy", "someemail@gmail.com")
